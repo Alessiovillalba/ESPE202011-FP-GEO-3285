34b54e0271a35fa59de60b77da034942422a3b22
Alessiovillalba <75390436+Alessiovillalba@users.noreply.github.com>
2021-02-12 13:37:12 -0500

    WS13-TASK

---
+++ assignments/UNIT2/LLLLLL/src/llllll/LLLLLL.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package llllll;
+
+/**
+ *
+ * @author Alessio
+ */
+public class LLLLLL {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        } else if (NetIncome >= 11212) {
+            if (NetIncome < 14205) {
+                SurplusFractionTax = 5;
+                value = (float) (NetIncome * 0.05);
+                 System.out.println("The Income Tax is-- "+value);
+
+            }else if (NetIncome >= 14205){
+                 if (NetIncome < 17854) {
+                SurplusFractionTax = 10;
+                value = (float) (NetIncome * 0.1);
+                 System.out.println("The Income Tax is-- "+value);
+                
+            }else if (NetIncome >= 17854){
+                 if (NetIncome < 21442) {
+                SurplusFractionTax = 12;
+                value = (float) (NetIncome * 0.12);
+                 System.out.println("The Income Tax is-- "+value);
+            }else if (NetIncome >= 21442){
+                 if (NetIncome < 42874) {
+                SurplusFractionTax = 15;
+                value = (float) (NetIncome * 0.15);
+                 System.out.println("The Income Tax is-- "+value);
+            }else if (NetIncome >= 42874){
+                 if (NetIncome < 64297) {
+                SurplusFractionTax = 20;
+                value = (float) (NetIncome * 0.2);   
+                 System.out.println("The Income Tax is-- "+value);
+            }else if (NetIncome >= 64297){
+                 if (NetIncome < 85729) {
+                SurplusFractionTax = 25;
+                value = (float) (NetIncome * 0.25);
+                 System.out.println("The Income Tax is-- "+value);
+            }else if (NetIncome >= 85729){
+                 if (NetIncome < 114288) {
+                SurplusFractionTax = 30;
+                value = (float) (NetIncome * 0.3);
+                 System.out.println("The Income Tax is-- "+value);
+            }else if (NetIncome >= 114288){
+                 SurplusFractionTax = 35;
+                value = (float) (NetIncome * 0.35);    
+                     System.out.println("The Income Tax is-- "+value);
+    }
+    
+}
